b79e9126bfc941d942e3c8c25e6004a0e23e2da8
Carmen <mcchristy10@yahoo.com>
2023-09-18 11:13:56 -0400

    updated readme and changed port

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,21 +21,6 @@ const typeDefs = gql`
     user: User
     profile: Profile
     }
-=======
-//     profile(_id: ID!): Profile
-//     notification(_id: ID!): Notification
-//     checkout(products: [ID]!): Checkout
-//   }
-
-//   type Mutation {
-//     addUser(firstName: String!, lastName: String!, email: String!, password: String!, registryTitle: String!): Auth
-//     addOrder(products: [ID]!): Order
-//     updateUser(firstName: String, lastName: String, email: String, password: String, registryTitle: String): User
-//     updateRegistryItem(_id: ID!, quantity: Int!): RegistryItem
-//     login(email: String!, password: String!): Auth
-// >>>>>>> main
-//   }
-  
 `;
 
 module.exports = typeDefs;
